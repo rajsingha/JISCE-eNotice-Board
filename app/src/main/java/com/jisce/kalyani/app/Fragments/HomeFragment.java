69774432c3c36db97398c01d2c216918fb4c78fe
Raj Singha <r.singha4520@gmail.com>
2019-08-07 01:53:10 +0530

    New Changes With Fragment

---
+++ app/src/main/java/com/jisce/kalyani/app/Fragments/HomeFragment.java
@@ -0,0 +1,99 @@
+package com.jisce.kalyani.app.Fragments;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jisce.kalyani.app.AllNoticeBoardAdapter;
+import com.jisce.kalyani.app.Model.Notices;
+import com.jisce.kalyani.app.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+
+    RecyclerView recyclerView;
+    AllNoticeBoardAdapter noticeBoardAdapter;
+    List<Notices> noticesList;
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        swipeRefreshLayout = view.findViewById(R.id.refreshRecycle);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        noticesList = new ArrayList<>();
+
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+        getData();
+    }
+
+
+
+
+
+    public void getData(){
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Notices");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    for (DataSnapshot noticesdataSnapshot: dataSnapshot.getChildren()){
+                        Notices not = noticesdataSnapshot.getValue(Notices.class);
+                        noticesList.add(not);
+
+
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                    noticeBoardAdapter= new AllNoticeBoardAdapter(getContext(),noticesList);
+                    recyclerView.setAdapter(noticeBoardAdapter);
+
+
+
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onRefresh() {
+        noticesList.clear();
+        getData();
+
+
+    }
+}
--- app/src/main/java/com/jisce/kalyani/app/MainActivity.java
+++ app/src/main/java/com/jisce/kalyani/app/MainActivity.java
@@ -1,85 +1,75 @@
 package com.jisce.kalyani.app;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.jisce.kalyani.app.Model.Notices;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.material.navigation.NavigationView;
+import com.jisce.kalyani.app.Fragments.HomeFragment;
 
-public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    RecyclerView recyclerView;
-    AllNoticeBoardAdapter noticeBoardAdapter;
-    List<Notices> noticesList;
-    SwipeRefreshLayout swipeRefreshLayout;
-
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        swipeRefreshLayout = findViewById(R.id.refreshRecycle);
-
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        noticesList = new ArrayList<>();
-
-        swipeRefreshLayout.setOnRefreshListener(this);
-
-        getData();
 
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open
+                ,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
-    public void getData(){
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Notices");
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()){
-                    for (DataSnapshot noticesdataSnapshot: dataSnapshot.getChildren()){
-                        Notices not = noticesdataSnapshot.getValue(Notices.class);
-                        noticesList.add(not);
 
 
-                        swipeRefreshLayout.setRefreshing(false);
-                    }
-                    noticeBoardAdapter= new AllNoticeBoardAdapter(MainActivity.this,noticesList);
-                    recyclerView.setAdapter(noticeBoardAdapter);
-
-
-
-                }
-
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-    }
 
     @Override
-    public void onRefresh() {
-        noticesList.clear();
-        getData();
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.nav_home:
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+                break;
+            case R.id.nav_feedback:
+                /*StartAppAd.showAd(this);
+                interstitialAd.loadAd();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FeedbackFragment()).commit();
+*/
+                break;
+
+            case R.id.nav_share:
+               /* Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareBody = "http://play.google.com/store/apps/details?id=com.gmoney.app";
+                sharingIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
+                startActivity(Intent.createChooser(sharingIntent,"Share Using"));*/
+                break;
+            case R.id.nav_exit:
+                openExitAlertDialogBuilder();
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
 
 
+        return true;
+    }
+
+    private void openExitAlertDialogBuilder() {
     }
 }
