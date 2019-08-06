b9aa919bfe4d85533579ad0ab724f76be5d1489a
Raj Singha <r.singha4520@gmail.com>
2019-08-07 02:50:29 +0530

    New Changes With Fragment

--- app/src/main/java/com/jisce/kalyani/app/MainActivity.java
+++ app/src/main/java/com/jisce/kalyani/app/MainActivity.java
@@ -33,6 +33,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 ,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+
+        if (savedInstanceState==null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+
+        }
     }
 
 
